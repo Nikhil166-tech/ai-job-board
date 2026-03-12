33272a2153229e9afbe35098f16fea44e792efb3
Nikhil166-tech <itharajunikhil16@gmail.com>
2026-03-12 09:30:46 +0530

    setuped express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("Backend server running"); 
+const express = require("express")
+
+const app = express()
+
+const PORT = 5000
+
+app.get("/", (req, res) => {
+    res.send("AI Job Board Backend Running")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
